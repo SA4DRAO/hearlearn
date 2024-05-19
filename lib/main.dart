690880443c22cf1f9f79616f5dc6d082f5385553
Adarsh Rao <adrao18020@gmail.com>
2024-05-19 10:08:43 +0530

    Fixed styling

--- lib/main.dart
+++ lib/main.dart
@@ -42,13 +42,13 @@ class _IntroScreenState extends State<IntroScreen>
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(seconds: 4), // Increased duration
+      duration: const Duration(seconds: 4),
       vsync: this,
     );
 
     _animation = CurvedAnimation(
       parent: _controller,
-      curve: Curves.easeInOut, // Smoother curve
+      curve: Curves.easeInOut,
     );
 
     _controller.addStatusListener((status) {
@@ -80,78 +80,104 @@ class _IntroScreenState extends State<IntroScreen>
         centerTitle: true,
         title: const Text('Amma'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/teach.png',
-              height: 500,
-            ), // Displaying the image
-            AnimatedBuilder(
-              animation: _animation,
-              builder: (context, child) {
-                return Opacity(
-                  opacity:
-                      _showEnglish ? 1.0 - _animation.value : _animation.value,
-                  child: Text(
-                    _showEnglish
-                        ? 'Press a button to start'
-                        : 'ప్రారంభించడానికి బటన్ నొక్కండి',
-                    style: const TextStyle(
-                        fontSize: 24, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                );
-              },
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                context.read<ListeningProvider>().setLocale(newLocale: 'te-IN');
-                context
-                    .read<CardsListProvider>()
-                    .loadData(jsonFilename: 'telugu.json');
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          bool isSmallScreen = constraints.maxWidth < 600;
+          double imageHeight = isSmallScreen ? 200 : 400;
+          double fontSize = isSmallScreen ? 16 : 24;
+          double buttonPaddingHorizontal = isSmallScreen ? 20 : 40;
+          double buttonPaddingVertical = isSmallScreen ? 10 : 20;
+          double buttonFontSize = isSmallScreen ? 16 : 20;
+          double spacing = isSmallScreen ? 20 : 40;
 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ImageListScreen(),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.green,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                textStyle: const TextStyle(fontSize: 20),
+          return SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/teach.png',
+                      height: imageHeight,
+                    ),
+                    SizedBox(height: spacing),
+                    AnimatedBuilder(
+                      animation: _animation,
+                      builder: (context, child) {
+                        return Opacity(
+                          opacity: _showEnglish
+                              ? 1.0 - _animation.value
+                              : _animation.value,
+                          child: Text(
+                            _showEnglish
+                                ? 'Press a button to start'
+                                : 'ప్రారంభించడానికి బటన్ నొక్కండి',
+                            style: TextStyle(
+                                fontSize: fontSize,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
+                        );
+                      },
+                    ),
+                    SizedBox(height: spacing),
+                    ElevatedButton(
+                      onPressed: () {
+                        context
+                            .read<ListeningProvider>()
+                            .setLocale(newLocale: 'te-IN');
+                        context
+                            .read<CardsListProvider>()
+                            .loadData(jsonFilename: 'telugu.json');
+
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ImageListScreen(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.green,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: buttonPaddingHorizontal,
+                            vertical: buttonPaddingVertical),
+                        textStyle: TextStyle(fontSize: buttonFontSize),
+                      ),
+                      child: const Text('తెలుగు'),
+                    ),
+                    SizedBox(height: spacing / 2),
+                    ElevatedButton(
+                      onPressed: () {
+                        context
+                            .read<ListeningProvider>()
+                            .setLocale(newLocale: 'en_US');
+                        context
+                            .read<CardsListProvider>()
+                            .loadData(jsonFilename: 'english.json');
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ImageListScreen(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.blue,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: buttonPaddingHorizontal,
+                            vertical: buttonPaddingVertical),
+                        textStyle: TextStyle(fontSize: buttonFontSize),
+                      ),
+                      child: const Text('English'),
+                    ),
+                  ],
+                ),
               ),
-              child: const Text('తెలుగు'),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                context.read<ListeningProvider>().setLocale(newLocale: 'en_US');
-                context
-                    .read<CardsListProvider>()
-                    .loadData(jsonFilename: 'english.json');
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ImageListScreen(),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.blue,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                textStyle: const TextStyle(fontSize: 20),
-              ),
-              child: const Text('English'),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
